30debbcdbbf6211304d20952261bc5c766804208
parabpratham <parabpratham@gmail.com>
2015-10-21 16:32:11 +0530

    1> Changed from drawing to adding the customoverays 2> Added listeners for the medialistplayer

--- src/com/vid/play/OverLayGenerator.java
+++ src/com/vid/play/OverLayGenerator.java
@@ -1,52 +1,43 @@
 package com.vid.play;
 
-import java.awt.Color;
-import java.awt.Font;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.Point;
-import java.awt.Rectangle;
-import java.awt.RenderingHints;
-import java.awt.Window;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JTextArea;
-import javax.swing.JWindow;
 import javax.swing.SwingUtilities;
 
-import com.sun.awt.AWTUtilities;
-import com.sun.jna.platform.WindowUtils;
 import com.vid.execute.AppLogger;
 import com.vid.log.trace.overlay.OverlayLog;
+import com.vid.test.CustomMediaPlayerFactory;
 
+import uk.co.caprica.vlcj.binding.internal.libvlc_media_t;
 import uk.co.caprica.vlcj.player.MediaPlayer;
 import uk.co.caprica.vlcj.player.MediaPlayerEventAdapter;
 import uk.co.caprica.vlcj.player.embedded.EmbeddedMediaPlayer;
+import uk.co.caprica.vlcj.player.list.MediaListPlayer;
+import uk.co.caprica.vlcj.player.list.MediaListPlayerEventAdapter;
 
 public class OverLayGenerator {
 
-	private Map<Integer, CustomeOverlays> overlays;
+	private static Map<Integer, CustomOverlay> overlays;
 
 	private boolean isTransperantWindowSupport;
 
-	private final EmbeddedMediaPlayer mediaPlayer;
+	private static final EmbeddedMediaPlayer mediaPlayer = CustomVideoPlayer.getMediaPlayer();
+	private static final MediaListPlayer mediaListPlayer = CustomMediaPlayerFactory.getMediaListPlayer();
+	private static final JFrame mainFrame = CustomVideoPlayer.getMainFrame();
 
 	// For displaying information in the information panel
-	private VideoInformationDisplayPanel videoInfoPanel;
+	private static final VideoInformationDisplayPanel videoInfoPanel = CustomVideoPlayer
+			.getVideoInformationDisplayPanel();;
 
 	private long currentTime;
 	private ThreadGroup tg;
 
-	private final JFrame mainFrame;
-
-	private final OverlayLog overalyTrace;
+	private final OverlayLog overalyTrace = AppLogger.getOverlayLog();;
 
 	/**
 	 * 
@@ -56,14 +47,6 @@ public class OverLayGenerator {
 	 * @param mainFrame
 	 */
 	public OverLayGenerator() {
-
-		// Initialize components
-		overalyTrace = AppLogger.getOverlayLog();
-		this.mediaPlayer = CustomeVideoPlayer.getMediaPlayer();
-		this.mainFrame = CustomeVideoPlayer.getMainFrame();
-		overlays = new HashMap<Integer, CustomeOverlays>();
-		this.videoInfoPanel = CustomeVideoPlayer.getVideoInformationDisplayPanel();
-
 		// Check if Transparent video overlay is supported
 		isTransperantWindowSupport = true;
 		try {
@@ -72,11 +55,38 @@ public class OverLayGenerator {
 			isTransperantWindowSupport = false;
 		}
 
+		// Start if the media is preloaded
+		startNewGeneratorFactory();
+
+		registerListeners();
+	}
+
+	public boolean isTransperantWindowSupport() {
+		return isTransperantWindowSupport;
+	}
+
+	public void setTransperantWindowSupport(boolean isTransperantWindowSupport) {
+		this.isTransperantWindowSupport = isTransperantWindowSupport;
+	}
+
+	private void startNewGeneratorFactory() {
+		overlays = new HashMap<Integer, CustomOverlay>();
 		tg = new ThreadGroup("Overlays Threads");
 		new Thread(tg, new OverlayFactory(), "Generate_Overlay").start();
 		overalyTrace.trace(tg.activeCount() + " threads in thread group.");
+	}
 
-		registerListeners();
+	@SuppressWarnings("deprecation")
+	public void stopVideoOerlays() {
+		overalyTrace.trace("StopVideo Called " + Calendar.getInstance().getTimeInMillis());
+		Thread thrds[] = new Thread[tg.activeCount()];
+		tg.enumerate(thrds);
+		for (Thread t : thrds) {
+			overalyTrace.trace(t.getName() + " Stopping " + t.getName());
+			if (t.isAlive())
+				t.stop();
+		}
+
 	}
 
 	private void registerListeners() {
@@ -91,35 +101,63 @@ public class OverLayGenerator {
 
 			@Override
 			public void stopped(MediaPlayer mediaPlayer) {
-				// TODO Auto-generated method stub
 				super.stopped(mediaPlayer);
+				enableOverlay(false);
+				stopVideoOerlays();
 			}
 
 			@Override
 			public void timeChanged(MediaPlayer mediaPlayer, long newTime) {
 				super.timeChanged(mediaPlayer, newTime);
-				new Thread(new DisplayOverlays(mediaPlayer, newTime), "Generate_Overlay").start();
+				enableOverlay(true);
+				new Thread(new DisplayOverlays(mediaPlayer, newTime), "Display_Overlay").start();
 			}
 
 			@Override
 			public void newMedia(MediaPlayer mediaPlayer) {
-				// TODO Auto-generated method stub
 				super.newMedia(mediaPlayer);
+				overalyTrace.trace("-- new media ----" + mediaPlayer.getTitle());
+			}
+
+			@Override
+			public void mediaChanged(MediaPlayer mediaPlayer, libvlc_media_t media, String mrl) {
+				super.mediaChanged(mediaPlayer, media, mrl);
+				overalyTrace.trace("-- media changed ----" + mrl);
+				enableOverlay(false);
+				if (mrl.equals("0")) {
+					stopVideoOerlays();
+					startNewGeneratorFactory();
+				}
+				enableOverlay(true);
 			}
 
 		});
 
-	}
+		mediaListPlayer.addMediaListPlayerEventListener(new MediaListPlayerEventAdapter() {
 
-	@SuppressWarnings("deprecation")
-	public void stopVideoOerlays() {
-		Thread thrds[] = new Thread[tg.activeCount()];
-		tg.enumerate(thrds);
-		for (Thread t : thrds) {
-			overalyTrace.trace(t.getName() + " Stopping " + t.getName());
-			if (t.isAlive())
-				t.stop();
-		}
+			@Override
+			public void stopped(MediaListPlayer mediaListPlayer) {
+				super.stopped(mediaListPlayer);
+				enableOverlay(false);
+				stopVideoOerlays();
+			}
+
+			@Override
+			public void nextItem(MediaListPlayer mediaListPlayer, libvlc_media_t item, String itemMrl) {
+				enableOverlay(false);
+				stopVideoOerlays();
+				super.nextItem(mediaListPlayer, item, itemMrl);
+				System.out.println("start nwe generator factory");
+				startNewGeneratorFactory();
+				enableOverlay(true);
+			}
+
+			@Override
+			public void mediaStateChanged(MediaListPlayer mediaListPlayer, int newState) {
+				// TODO Auto-generated method stub
+				super.mediaStateChanged(mediaListPlayer, newState);
+			}
+		});
 
 	}
 
@@ -137,37 +175,46 @@ public class OverLayGenerator {
 
 		@Override
 		public void run() {
+
+			while (getOverlays() == null || getOverlays().size() == 0)
+				try {
+					Thread.sleep(10000);
+				} catch (InterruptedException e1) {
+					e1.printStackTrace();
+				}
+
 			currentTime = mediaPlayer.getTime();
 			List<Integer> keySet = new ArrayList<Integer>();
-			keySet.addAll(overlays.keySet());
+			keySet.addAll(getOverlays().keySet());
 			java.util.Collections.sort(keySet);
 
 			synchronized (overlay) {
-				if (overlays.size() == 0)
+				if (getOverlays().size() == 0)
 					mediaPlayer.setOverlay(null);
 				else {
 					Outer: for (int startTime : keySet) {
 						// System.out.println(Thread.currentThread().getName() +
 						// " " + startTime + " " + currentTime);
 						overalyTrace.trace(Thread.currentThread().getName() + " " + startTime + " " + currentTime);
-						CustomeOverlays customeOverlay = overlays.get(startTime);
+						CustomOverlay customeOverlay = getOverlays().get(startTime);
 						// TODO replace with appropriate
 						if (startTime <= currentTime + 267) {
 							if (customeOverlay == null) {
 								break;
 							}
-							mediaPlayer.setOverlay(customeOverlay == null ? null : customeOverlay.test);
+							mediaPlayer.setOverlay(customeOverlay == null ? null : customeOverlay);
 							mediaPlayer.enableOverlay(true);
-							overlays.remove(customeOverlay.startTime);
+							getOverlays().remove(customeOverlay.getStartTime());
 							try {
 								// System.out.println(Thread.currentThread().getName()
 								// + " " + customeOverlay.endTime + " "
 								// + mediaPlayer.getTime() + " " + currentTime +
 								// " " + mediaPlayer.getTime());
-								overalyTrace.trace(Thread.currentThread().getName() + " " + customeOverlay.endTime + " "
-										+ mediaPlayer.getTime() + " " + currentTime + " " + mediaPlayer.getTime());
-								int n = (int) (customeOverlay.endTime - mediaPlayer.getTime()) / 1000;
-								while (customeOverlay.endTime >= mediaPlayer.getTime()
+								overalyTrace.trace(Thread.currentThread().getName() + " " + customeOverlay.getEndTime()
+										+ " " + mediaPlayer.getTime() + " " + currentTime + " "
+										+ mediaPlayer.getTime());
+								int n = (int) (customeOverlay.getEndTime() - mediaPlayer.getTime()) / 1000;
+								while (customeOverlay.getEndTime() >= mediaPlayer.getTime()
 										&& currentTime <= mediaPlayer.getTime()) {
 									if (n == 0)
 										n = 1;
@@ -177,8 +224,8 @@ public class OverLayGenerator {
 									// mediaPlayer.getTime()) / n) + " "
 									// + mediaPlayer.getTime() + " " +
 									// customeOverlay.endTime);
-									Thread.sleep((customeOverlay.endTime - mediaPlayer.getTime()) / n);
-									n = (int) (customeOverlay.endTime - mediaPlayer.getTime()) / 1000;
+									Thread.sleep((customeOverlay.getEndTime() - mediaPlayer.getTime()) / n);
+									n = (int) (customeOverlay.getEndTime() - mediaPlayer.getTime()) / 1000;
 
 								}
 							} catch (InterruptedException e) {
@@ -187,8 +234,8 @@ public class OverLayGenerator {
 							} finally {
 								mediaPlayer.setOverlay(null);
 							}
-						} else if (customeOverlay.endTime < currentTime + 267) {
-							overlays.remove(startTime);
+						} else if (customeOverlay.getEndTime() < currentTime + 267) {
+							getOverlays().remove(startTime);
 						}
 						break Outer;
 					}
@@ -237,15 +284,15 @@ public class OverLayGenerator {
 					@Override
 					public void run() {
 						try {
-							if (overlays.isEmpty() || overlays.get(currentTime) == null) {
-								// System.out.println(Thread.currentThread().getName()
-								// + " Overlay generated " + i);
+							// System.out.println(Thread.currentThread().getName()
+							// + " Overlay generated " + i);
+							if (currentTime < 1000) {
 								overalyTrace.trace(Thread.currentThread().getName() + " Overlay generated " + i);
-								if (i == 0 && currentTime < 20000)
-									overlays.put(20000, new CustomeOverlays(20000, 1200000, draw()));
-								else if (currentTime < 1000)
-									overlays.put(1000, new CustomeOverlays(1000, 30000, draw2()));
-								i++;
+								getOverlays().put(1000, new CustomOverlay(null, 1000, 19000, i));
+							}
+							if (currentTime < 25000) {
+								overalyTrace.trace(Thread.currentThread().getName() + " Overlay generated " + i);
+								getOverlays().put(25000, new CustomOverlay(null, 25000, 30000000, i));
 							}
 						} catch (Exception e) {
 							e.printStackTrace();
@@ -280,147 +327,18 @@ public class OverLayGenerator {
 		}
 	}
 
-	private Window draw2() {
-		Overlay overlay = new Overlay(null);
-		return overlay;
-	}
-
-	private Window draw() {
-		JWindow test = null;
-		if (isTransperantWindowSupport) {
-			test = new JWindow(null, WindowUtils.getAlphaCompatibleGraphicsConfiguration()) {
-				private static final long serialVersionUID = 1L;
-
-				@Override
-				public void paint(Graphics g) {
-					Graphics2D g2 = (Graphics2D) g;
-
-					g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-					g2.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING,
-							RenderingHints.VALUE_TEXT_ANTIALIAS_LCD_HRGB);
-
-					g.setColor(Color.white);
-					g.drawRect(100, 150, 100, 100);
-
-					g.setFont(new Font("Sans", Font.BOLD, 32));
-					g.drawString("Heavyweight overlay test", 100, 300);
-				}
-			};
-
-			AWTUtilities.setWindowOpaque(test, false); // Doesn't work in
-														// full-screen
-														// exclusive
-														// mode, you would
-														// have
-														// to use
-														// 'simulated'
-														// full-screen -
-														// requires
-														// Sun/Oracle
-														// JDK
-			test.setBackground(new Color(0, 0, 0, 0)); // This is what you
-														// do in
-
-			JTextArea superImposedLightweigtLabel = new JTextArea("Hello, VLC.");
-			superImposedLightweigtLabel.setOpaque(false);
-			test.getContentPane().add(superImposedLightweigtLabel);
-			superImposedLightweigtLabel.setVisible(true);
-
-		}
-		return test;
-	}
-
-	private Window draw1() {
-		JWindow test = null;
-		if (isTransperantWindowSupport) {
-			test = new JWindow(null, WindowUtils.getAlphaCompatibleGraphicsConfiguration()) {
-				private static final long serialVersionUID = 1L;
-
-				@Override
-				public void paint(Graphics g) {
-					Graphics2D g2 = (Graphics2D) g;
-
-					g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-					g2.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING,
-							RenderingHints.VALUE_TEXT_ANTIALIAS_LCD_HRGB);
-
-					g.setColor(Color.red);
-					g.drawRect(200, 250, 100, 100);
-
-					g.setFont(new Font("Sans", Font.BOLD, 32));
-					g.drawString("Heavyweight overlay test", 100, 100);
-				}
-			};
-
-			AWTUtilities.setWindowOpaque(test, false); // Doesn't work in
-														// full-screen
-														// exclusive
-														// mode, you would
-														// have
-														// to use
-														// 'simulated'
-														// full-screen -
-														// requires
-														// Sun/Oracle
-														// JDK
-			test.setBackground(new Color(0, 0, 0, 0)); // This is what you
-														// do in
-
-			final JLabel superImposedLightweigtLabel = new JLabel("Hello, VLC.", JLabel.CENTER);
-			superImposedLightweigtLabel.setOpaque(false);
-			test.getContentPane().add(superImposedLightweigtLabel);
-			superImposedLightweigtLabel.setVisible(true);
-		}
-
-		test.addMouseListener(new MouseAdapter() {
-
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				Point point = e.getPoint();
-				overalyTrace.trace("Ovelay mouse Clicked " + point);
-				System.out.println("Ovelay mouse Clicked " + point);
-
-				if (new Rectangle(200, 250, 100, 100).contains(point)) {
-					overalyTrace.trace("Ovelay mouse showed " + point);
-					videoInfoPanel.setTitleText(" Hi rectangle " + point);
-				}
-				if (new Rectangle(100, 80, 10000, 20).contains(point)) {
-					overalyTrace.trace("Ovelay mouse entered " + point);
-					videoInfoPanel.setTitleText(" Hi string " + point);
-				}
-
-			}
-
-			@Override
-			public void mouseEntered(MouseEvent e) {
-				Point point = e.getPoint();
-				overalyTrace.trace("Ovelay mouse entered " + point);
-				// System.out.println("Ovelay mouse entered " + point);
-				super.mouseEntered(e);
-			}
-		});
-
-		return test;
-	}
-
 	public static void NotifyObj() {
 		synchronized (obj) {
 			obj.notifyAll();
 		}
 	}
 
-	private class CustomeOverlays {
+	public static Map<Integer, CustomOverlay> getOverlays() {
+		return overlays;
+	}
 
-		private Window test;
-		private int startTime;
-		private int endTime;
-
-		public CustomeOverlays(int startTime, int endTime, Window test) {
-			this.startTime = startTime;
-			this.endTime = endTime;
-			this.test = test;
-
-		}
+	public void enableOverlay(boolean b) {
+		mediaPlayer.enableOverlay(b);
 	}
 
 }
