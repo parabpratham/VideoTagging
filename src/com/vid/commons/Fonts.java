3a531ff0123d2b5a509e6e8c756dec1eff945555
parabpratham <parabpratham@gmail.com>
2015-10-24 03:09:56 +0530

    hi

--- src/com/vid/commons/Fonts.java
+++ src/com/vid/commons/Fonts.java
@@ -4,6 +4,6 @@ import java.awt.Font;
 
 public class Fonts {
 
-	public static final Font SANSERIF = new Font("Sansserif", Font.BOLD, 18);;
+	public static final Font SANSERIF = new Font("Sansserif", Font.BOLD, 18);
 
 }
