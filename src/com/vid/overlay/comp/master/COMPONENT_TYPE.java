cc25cbe304bf5b73edf94ed4c14b724af0efc845
parabpratham <parabpratham@gmail.com>
2015-10-28 23:24:00 +0530

    Updated the enum files to fetch from name

--- src/com/vid/overlay/comp/master/COMPONENT_TYPE.java
+++ src/com/vid/overlay/comp/master/COMPONENT_TYPE.java
@@ -2,5 +2,14 @@ package com.vid.overlay.comp.master;
 
 public enum COMPONENT_TYPE {
 
-	JCONPONENT, SHAPE, IMAGE, ENTIREVIDEOCOMMENT, ANNOTATIONMARKER;
+	JCOMPONENT, SHAPE, IMAGE, ENTIREVIDEOCOMMENT, ANNOTATIONMARKER;
+
+	public static COMPONENT_TYPE getFromName(String name) {
+		COMPONENT_TYPE[] values = values();
+		for (COMPONENT_TYPE component_TYPE : values) {
+			if (component_TYPE.name().equalsIgnoreCase(name))
+				return component_TYPE;
+		}
+		return null;
+	}
 }
--- src/com/vid/overlay/comp/master/SHAPE_TYPE.java
+++ src/com/vid/overlay/comp/master/SHAPE_TYPE.java
@@ -4,4 +4,13 @@ public enum SHAPE_TYPE {
 
 	RECTANGLE, LINE, OVAL, POLYGON, STRING, IMAGE, ARROW, ROUNDED_RECTANGLE;
 
+	public static SHAPE_TYPE getFromName(String name) {
+		SHAPE_TYPE[] values = values();
+		for (SHAPE_TYPE jcomponent_TYPE : values) {
+			if (jcomponent_TYPE.name().equalsIgnoreCase(name))
+				return jcomponent_TYPE;
+		}
+		return null;
+	}
+
 }
