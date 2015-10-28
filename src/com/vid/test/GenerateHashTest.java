0c1c49ba2501cb084882d882a45888aa027c06a7
parabpratham <parabpratham@gmail.com>
2015-10-28 23:29:04 +0530

    HashTest

---
+++ src/com/vid/test/GenerateHashTest.java
@@ -0,0 +1,44 @@
+package com.vid.test;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class GenerateHashTest {
+
+	public static void main(String[] args) throws NoSuchAlgorithmException {
+		File file = new File("J:/Movies/Hollywood/we are legion the story of the hacktivists 2012 dvdrip x264.avi");
+		//File file = new File("k:/Install/Study/Programming/SpringWorkspace/VideoPlayer/src/resources/overlay_xml/input .xml");
+		
+		System.out.println(file.hashCode());
+		try {
+			MessageDigest md = MessageDigest.getInstance("SHA1");
+			FileInputStream fis = new FileInputStream(file);
+
+			byte[] dataBytes = new byte[512*1024];
+
+			int nread = 0;
+			int totalRead=0;
+			while ((nread = fis.read(dataBytes)) != -1) {
+				md.update(dataBytes, 0, nread);
+			}
+			;
+
+			byte[] mdbytes = md.digest();
+
+			// convert the byte to hex format
+			StringBuffer sb = new StringBuffer("");
+			for (int i = 0; i < mdbytes.length; i++) {
+				sb.append(Integer.toString((mdbytes[i] & 0xff) + 0x100, 16).substring(1));
+			}
+
+			System.out.println("Digest(in hex format):: " + sb.toString());
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
